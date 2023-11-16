4d8c3b61af1c8e22c499e28e0767e8acc6b9d161
Jonathan Tyler Weber <jonathantweber@gmail.com>
2023-11-16 18:16:31 +0800

    setting up board

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, session
 from boggle import Boggle
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+
+debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+@app.route('/')
+def index():
+    board = boggle_game.make_board()
+    session['board'] = board
+    return render_template('index.html', board=board)
+
